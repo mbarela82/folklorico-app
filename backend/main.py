03763732b3690e8087077bc884359fcbb51a0126
mbarela82 <mbarela82@gmail.com>
2025-12-22 14:26:05 -0700

    Fixing railway deployment issues

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    # This points to the application we created in app/main.py
-    # reload=True means the server restarts automatically when you save code changes
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
